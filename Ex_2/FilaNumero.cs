ea386cb2f8cff17e1db6e4418140d436a7307957
thavao <othavioact@gmail.com>
2024-05-04 14:16:13 -0300

    Criando classes do Exercício 2

---
+++ Ex_2/FilaNumero.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex_2
+{
+    internal class FilaNumero
+    {
+        Numero cabeca;
+        Numero cauda;
+        int maior;
+        int menor;
+        float media;
+
+        public FilaNumero()
+        {
+            cabeca = null;
+            cauda = null;
+        }
+        bool Vazio()
+        {
+            return cabeca == null;
+        }
+        public void Push(Numero aux)
+        {
+            if (Vazio())
+            {
+                cabeca = aux;
+                cauda = aux;
+            }
+            else
+            {
+                cauda.setProximo(aux);
+                cauda = aux;
+            }
+        }
+        public void Pop()
+        {
+            if (!Vazio())
+            {
+                cabeca = cabeca.getProximo();
+            }
+        }
+    }
+}
---
+++ Ex_2/Numero.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex_2
+{
+    internal class Numero
+    {
+        int valor;
+        Numero proximo;
+
+        public Numero(int num)
+        {
+            valor = num;
+            proximo = null;
+        }
+        public void setProximo(Numero recebido)
+        {
+            proximo = recebido;
+        }
+        public Numero getProximo()
+        {
+            return proximo;
+        }
+        public int getNumero()
+        {
+            return valor;
+        }
+    }
+}
